012b2ea8e7701af08ad654568502ee38c6ec293b
beyardev <beyardev@gmail.com>
2022-11-29 10:28:27 +0000

    add more tests

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -12,4 +12,19 @@ describe('calculateLBTT',()=>{
         expect(calculateLBTT(195000)).toBe("£1000 tax due")
         expect(calculateLBTT(250000)).toBe("£2100 tax due")
     })
+    test("should return 5% tax of house price from 250001-325k plus the 2%",()=>{
+        expect(calculateLBTT(250001)).toBe("£2100.05 tax due")
+        expect(calculateLBTT(260000)).toBe("£2600 tax due")
+        expect(calculateLBTT(325000)).toBe("£5850 tax due")
+    })
+    test("should return 10% tax of house price from 325001-750k plus 5% plus 2%",()=>{
+        expect(calculateLBTT(325001)).toBe("£5850.1 tax due")
+        expect(calculateLBTT(625000)).toBe("£35850 tax due")
+        expect(calculateLBTT(750000)).toBe("£48350 tax due")
+    })
+    test("should return 12% tax of house price from 750001 and up, plus 10%,5%,2%",()=>{
+        expect(calculateLBTT(750001)).toBe("£48350.12 tax due")
+        expect(calculateLBTT(1000000)).toBe("£78350 tax due")
+        expect(calculateLBTT(1200995)).toBe("£102469.4 tax due")
+    })
 })
