c429f030fb13fba1d7893ff4331036149a89c1a2
beyarDev <beyarDev@gmail.com>
2023-01-09 12:27:25 +0000

    add comments

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+//assumptions:
+// The buyer of the house currently owns a property and lives in it as their main residence
+// The buyer does not conduct any kind of business activity from their house i.e.
+//it is purely for personal use
+// The buyer does not own any other properties
+// At the end of the purchase the buyer will still only own one house i.e. they
+//intent to sell their current home at the same date they buy the new one
+
 function calculateLBTT(housePrice) {
   let tax = 0;
   const threshold = 145_000;
@@ -8,9 +16,9 @@ function calculateLBTT(housePrice) {
   const band3 = 750_000;
   const band3TaxPercentage = 10;
   const finalBand = 12;
-  const band1FullTax = ((band1 - threshold) * band1TaxPercentage) / 100; // 2100
-  const band2FullTax = ((band2 - band1) * band2TaxPercentage) / 100; // 3750
-  const band3FullTax = ((band3 - band2) * band3TaxPercentage) / 100; // 42500
+  const band1FullTax = ((band1 - threshold) * band1TaxPercentage) / 100;
+  const band2FullTax = ((band2 - band1) * band2TaxPercentage) / 100;
+  const band3FullTax = ((band3 - band2) * band3TaxPercentage) / 100;
   if (housePrice <= threshold) {
     return tax;
   } else if (housePrice <= band1) {
